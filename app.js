f27674aa7b5df104f76097a6ce37f23a34d809fc
jiwon03 <bjiwon03@gmail.com>
2020-11-02 20:22:29 +0900

    Express core: Routing

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from "express";
-import morgan from "morgan"; //logger
+import morgan, { format } from "morgan"; //logger
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express()
 
-const PORT = 4000;
-
-const handleListening = () => 
-    console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('Hello from my home');
 const handleProfile = (req, res) => res.send("You are on my profile");
 
@@ -18,6 +15,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.listen(PORT, handleListening);
+
+app.use("/user", userRouter);
+
+export default app;
