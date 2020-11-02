947cf14b485f1874fc2bd287de9cc4fc7aaa5088
jiwon03 <bjiwon03@gmail.com>
2020-11-02 17:03:46 +0900

    Express core: Middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan"; //logger
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express()
 
 const PORT = 4000;
@@ -8,6 +13,11 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send('Hello from my home');
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
