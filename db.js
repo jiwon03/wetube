51d87d20a7baab88388d66d3441529ac46846573
jiwon03 <bjiwon03@gmail.com>
2020-11-10 00:13:25 +0900

    Home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+{
+    id:324393,
+    title: "Video awsome",
+    description: "This is something I love",
+    views:24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+},
+{
+    id:324325,
+    title: "Video super",
+    description: "This is something I love",
+    views:24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+},
+{
+    id:121212,
+    title: "Video nice",
+    description: "This is something I love",
+    views:24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+},
+{
+    id:87566,
+    title: "Video perfect",
+    description: "This is something I love",
+    views:24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+}
+];
