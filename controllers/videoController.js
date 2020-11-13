08eb55a1105a7ae5f4d6f8d940edcf9b1dd003e5
jiwon03 <bjiwon03@gmail.com>
2020-11-14 00:56:26 +0900

    More controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"
+import routes from "../routes";
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
@@ -8,7 +9,14 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description}
+    } = req;
+    // To do: Upload and save video
+    res.redirect(routes.videoDetail(324393))
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 
