3408c0750535698e51a0c9981005571802faed18
jiwon03 <bjiwon03@gmail.com>
2020-11-18 00:43:46 +0900

    Uploading and Creating a Video part2

--- app.js
+++ app.js
@@ -11,13 +11,9 @@ import globalRouter from "./routers/globalRouter";
 
 const app = express()
 
-//app.use( helmet({ contentSecurityPolicy: false, })); 
 app.use(helmet());
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
-    return next();
-    }); //임시
 app.set('view engine', "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
