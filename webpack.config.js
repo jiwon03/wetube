d1e4058d95e7455c020a3448fe232310116042db
jiwon03 <bjiwon03@gmail.com>
2020-12-02 23:50:10 +0900

    Styles with Webpack part2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
@@ -11,24 +12,47 @@ const config = {
   module: {
     rules: [
       {
-        test: /\(.scss)$/,
-        use: ExtractCSS.extract([
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      browsers: "cover 99.5%",
+                    },
+                  ],
+                ],
+              },
+            },
           },
           {
             loader: "sass-loader",
           },
-        ]),
+        ],
       },
     ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+    }),
+  ],
 };
+
 module.exports = config;
