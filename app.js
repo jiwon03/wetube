7bc397f0a4809faa7bc7370108119edc4794ad08
jiwon03 <bjiwon03@gmail.com>
2021-01-20 21:07:51 +0900

    MongoStore and Middlewares

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose, { mongo } from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+
 import "./passport";
 
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -27,6 +32,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
   })
 );
 app.use(passport.initialize());
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,27 +1,28 @@
 import express from "express";
 import routes from "../routes";
-import { videos, 
-    getUpload,
-    postUpload, 
-    videoDetail, 
-    getEditVideo, 
-    postEditVideo,
-    DeleteVideo } from "../controllers/videoController";
-import { uploadVideo } from "../middlewares";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  getEditVideo,
+  postEditVideo,
+  DeleteVideo,
+} from "../controllers/videoController";
+import { onlyPrivate, uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 // Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 // Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 // Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo(), DeleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, DeleteVideo);
 
 export default videoRouter;
