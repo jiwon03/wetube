182dc36a75be20c06901940e5205341fd6b4f798
jiwon03 <bjiwon03@gmail.com>
2021-02-09 01:12:57 +0900

    Adding Creator to Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -36,7 +36,10 @@ export const postUpload = async (req, res) => {
     fileUrl: path,
     title,
     description,
+    creator: req.user.id,
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -45,7 +48,8 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
--- models/Comment.js
+++ models/Comment.js
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: "Text is required"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-    // video: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref:"Video"
-    // } 이 방법도 있음
+  text: {
+    type: String,
+    required: "Text is required",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const model = mongoose.model("Comment", CommentSchema);
 
 export default model;
-
