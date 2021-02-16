8377c73524160140a210fe8310831a1409053a27
jiwon03 <bjiwon03@gmail.com>
2021-02-16 19:24:41 +0900

    Mute Unmute

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,15 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
+    "no-console": "off",
+  },
+  env: {
+    browser: true,
   },
 };
